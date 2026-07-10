592dde4afbcf78f179e495593a23588dd53dfedf
jessica <epunamjessicaoge@gmail.com>
2026-07-10 22:48:08 +0100

    add hospital patient intake script

--- day1.py
+++ day1.py
@@ -1,2 +1,9 @@
-print("hello jessica")
-print("future AI biology expert")
+ # Hospital Patient Registration system
+patient_name = "John Smith"
+age = 20
+is_new_patient = True
+# Printing the patient profile summary
+print("---Hospital Patient profile---")
+print("Name:", patient_name)
+print("Age:", age)
+print("New Patint Status:", is_new_patient)
